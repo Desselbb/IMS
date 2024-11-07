2df24d98760de45dc2771f3a7ecac29fa661f8a1
Desselbb <136103220+Desselbb@users.noreply.github.com>
2024-11-07 12:00:51 +0200

    Added SideMenu, Header and Footer

--- IMS/Program.cs
+++ IMS/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Components.Web;
 using IMSClassLibrary.Repos;
 using IMSClassLibrary.repos;
 using Microsoft.EntityFrameworkCore;
+using Blazored.SessionStorage;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -28,9 +29,24 @@ builder.Services.AddTransient<ProfileModuleRepository>();
 builder.Services.AddTransient<EmailRepository>();
 builder.Services.AddTransient<ModuleRepository>();
 builder.Services.AddTransient<ProfileRepository>();
+builder.Services.AddTransient<NotificationService>();
+builder.Services.AddTransient<Authorisation>();
+builder.Services.AddBlazoredSessionStorage();
+
+
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(300);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -41,10 +57,18 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-
 app.UseRouting();
+app.UseSession();
+app.Use(async delegate (HttpContext context, Func<Task> next)
+{
+    //this throwaway session variable will "prime" the setstring() method
+    //to allow it to be called after the response has started
+    var tempkey = Guid.NewGuid().ToString(); //create a random key
+    context.Session.Set(tempkey, Array.Empty<byte>()); //set the throwaway session variable
+    context.Session.Remove(tempkey); //remove the throwaway session variable
+    await next(); //continue on with the request
+});
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
--- IMSClassLibrary/Interfaces/IDepartment.cs
+++ IMSClassLibrary/Interfaces/IDepartment.cs
@@ -1,8 +1,4 @@
-﻿using IMSClassLibrary.Models;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace IMSClassLibrary.Interfaces
 {
